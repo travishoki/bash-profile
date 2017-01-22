96257810acb37fb873f1833cfaed868ec8ad8521
Travis Hoki <travishoki@gmail.com>
2017-01-21 22:37:20 -0700

    edit tlpweb

--- tlp.sh
+++ tlp.sh
@@ -7,7 +7,6 @@ function tlpweb(){
   	"https://drive.google.com/drive/u/0/my-drive"
   	"https://calendar.google.com/calendar/render?tab=oc"
   	"https://www.17hats.com/"
-  	"http://s2.quickmeme.com/img/b6/b6f682a6dd0c1db9223c0b955482b8ef8695662558407002ffb334a0cd60eacf.jpg"
   	)
   for i in "${TEMPAPPARRAY[@]}"; do
   	echo "Open "$i
